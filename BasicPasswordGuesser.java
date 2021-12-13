85d80edf1f2c51e399f1eac68eff32a4796ee507
Sarthak Gupta <16374350+sarthakgup@users.noreply.github.com>
2021-12-13 00:13:50 -0600

    Create BasicPasswordGuesser.java

---
+++ BasicPasswordGuesser.java
@@ -0,0 +1,21 @@
+//simple user-input driven scanner intro from high school data structures
+public class Program
+{
+  public static int main(String[] args) 
+  {
+    Scanner input = new Scanner(System.in);
+    password = Bunnies are fluffy;
+    
+    System.out.print("Enter password: ")
+    String guess = input.nextLine;
+
+    if (guess = password)
+    {
+       System.out.Println("You may enter");
+    }
+    else
+    {
+       System.out.Println("Wrong!!");
+    }
+  }    
+}
